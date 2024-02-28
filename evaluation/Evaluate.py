9a512983f94a097cd11563bd2f61ef87757dc468
Roberto Villegas <60374178+rv3801@users.noreply.github.com>
2024-02-28 12:59:00 -0800

    Add "PUUID" Retrieval
    Added code to get user's PUUID using API call. API key retrieval from separate (ignored by git) also added.

--- evaluation/Evaluate.py
+++ evaluation/Evaluate.py
@@ -1,12 +1,15 @@
 from riotwatcher import LolWatcher
 import regex
+from pathlib import Path
+from requests import session
+import json
 
-def validate_player(player):
+def validate_name(name):
 	# A Riot ID can contain between 3-16 characters before the # and
 	# between 3-5 characters after. These characters can be any
 	# Unicode letter. This regular expression checks if the given 
 	# name matches this pattern and returns False if not correct.
-	if (regex.match("^\p{Letter}{3,16}#\p{Letter}{3,5}$", player)):
+	if (regex.match("^\p{Letter}{3,16}#\p{Letter}{3,5}$", name)):
 		return True
 	print("Invalid name, try again.")
 	return False
@@ -16,9 +19,25 @@ def evaluate_start():
 	# then loops until a valid ID is passed through.
 	valid_name = False
 	while(not valid_name):
-		player_name = input("Enter the player's Riot ID with tag (ex. \"name#tag\"): ")
-		valid_name = validate_player(player_name)
-	
-	print(player_name)
+		user_name = input("Enter the player's Riot ID with tag (ex. \"name#tag\"): ")
+		valid_name = validate_name(user_name)
 
+	# After the name is verified, start the evaluation process by getting
+	# a list of the player's TODO most recent games.
+	user_game_name = user_name[:user_name.find("#")]
+	user_tag = user_name[user_name.find("#") + 1:]
+
+	# Uses Path() to avoid working directory issues with finding the .txt file
+	filepath = Path(__file__).parent / "RiotAPIKey.txt"
+	with open(filepath, "r") as f:
+		api_key = f.read()
+	lol_watcher = LolWatcher(api_key)
+
+	# API call for unsupported endpoint
+	this_session = session()
+	user_puuid = this_session.get(
+		f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{user_game_name}/{user_tag}",
+		headers={"X-Riot-Token": api_key}
+	).json()["puuid"]
+	
 evaluate_start()
